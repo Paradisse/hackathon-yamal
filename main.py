22b84a572dc96d0264078873ab9ef307945be9d4
Paradise <shinodise@gmail.com>
2021-10-17 03:21:06 +0300

    cleared db, changed responses and added reg check

--- main.py
+++ main.py
@@ -7,17 +7,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from marshmallow import Schema, fields
-
-
-class Serializer(object):
-    def serialize(self):
-        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
-
-    @staticmethod
-    def serialize_list(l):
-        return [m.serialize() for m in l]
 
 
 app = Flask(__name__)
@@ -29,6 +20,15 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = "True"
 db = SQLAlchemy(app)
 engine = create_engine("sqlite:///hackathon.db", echo=True)
 Session = sessionmaker(bind=engine)
+
+
+class Serializer(object):
+    def serialize(self):
+        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+    @staticmethod
+    def serialize_list(l):
+        return [m.serialize() for m in l]
 
 
 class UserModel(db.Model, Serializer):
@@ -72,24 +72,29 @@ def login(user_email, user_pass):
         data = schema.dump(response)
         return make_response(data, 200)
     except NoResultFound:
-        data = {"message": "No such user in db", "code": "ERROR"}
-        return make_response(json.dumps(data), 404)
+        data = {"message": "No such user with this email and password", "code": "ERROR"}
+        return make_response(json.dumps(data), 400)
+    except MultipleResultsFound:
+        data = {"message": "Database is corrupted", "code": "DB_ERROR"}
+        return make_response(json.dumps(data), 400)
 
 
 @app.route("/register/<string:user_email>/<string:user_pass>", methods=["POST"])
 @cross_origin()
 def register(user_email, user_pass):
-    try:
-        # TODO: add parsing args
+    # TODO: add parsing args
+    res = session.query(UserModel).filter(UserModel.email == user_email).first()
+    if res is not None:
+        data = {"message": "User with email=" + user_email + " already exists", "code": "ERROR"}
+        return make_response(json.dumps(data), 200)
+    else:
         hash_pass = hashlib.sha256(user_pass.encode("utf-8")).hexdigest()
         user = UserModel(email=user_email, password=hash_pass, first_name=None, second_name=None, patronymic=None)
         db.session.add(user)
         db.session.commit()
-        data = {"message": "User created", "code": "SUCCESS"}
+        s = schema.dump(user)
+        data = {"message": "User created", "code": "SUCCESS", "user": s}
         return make_response(json.dumps(data), 201)
-    except TimeoutError as e:
-        print(e)
-        abort(400)
 
 
 if __name__ == "__main__":
